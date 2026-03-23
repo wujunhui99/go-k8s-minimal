113b5597caafc1569da318519e64080afdc2dd3b
wujunhui99 <53573423+wujunhui99@users.noreply.github.com>
2026-03-23 12:32:32 +0800

    feat: add config endpoint (#1)

---
+++ cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os" // 新增
+)
+
+// 见鬼了，这个文件是干嘛的？
+func main() {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+
+	// 新增接口
+	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
+		app := os.Getenv("APP_NAME")
+		if app == "" {
+			app = "default-app"
+		}
+		fmt.Fprintf(w, "app_name=%s", app)
+	})
+
+	fmt.Println("Server starting on :8080...")
+	http.ListenAndServe(":8080", nil)
+}
